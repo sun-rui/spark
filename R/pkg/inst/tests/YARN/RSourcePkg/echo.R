9f88095e803dc8a2de5c70e0be11ef9b1ecdb484
Sun Rui <run.sun@intel.com>
2015-11-11 15:52:04 +0800

    Add yarn test files.

---
+++ R/pkg/inst/tests/YARN/RSourcePkg/echo.R
@@ -0,0 +1,7 @@
+Sys.getenv("R_PROFILE_USER")
+Sys.getenv("SPARKR_PACKAGE_DIR")
+.First
+.libPaths()
+cat("hello, yarn")
+library("sparkPackageTest")
+myfunc()
---
+++ R/pkg/inst/tests/YARN/echo.R
@@ -0,0 +1,5 @@
+Sys.getenv("R_PROFILE_USER")
+Sys.getenv("SPARKR_PACKAGE_DIR")
+.First
+.libPaths()
+cat("hello, yarn")
